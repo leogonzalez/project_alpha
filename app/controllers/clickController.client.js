3b2dee6e7cc83cd710587d20d5232257112fd3cf
leogonzalez <leo.lujan.gonzalez@gmail.com>
2016-05-14 22:28:58 -0300

    final commit for the first phase of the Clementine.js stack

--- app/controllers/clickController.client.js
+++ app/controllers/clickController.client.js
@@ -2,13 +2,13 @@
 
 (function(){
   var addButton = document.querySelector('.btn-add');
-  var deleteButton = document.querySelector('btn-delete');
+  var deleteButton = document.querySelector('.btn-delete');
   var clickNbr = document.querySelector('#click-nbr');
   var apiUrl = 'http://localhost:3000/api/clicks';
 
   function ready(fn){
 
-    if typeof fn!== 'function' {
+    if (typeof fn!== 'function') {
       return;
     }
 
@@ -18,6 +18,44 @@
 
     document.addEventListener('DOMContentLoaded',fn,false);
 
+  } // closes ready function
+
+  function ajaxRequest(method,url,callback){
+
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function(){
+      if (xmlhttp.readyState === 4 && xmlhttp.status ===200) {
+        callback(xmlhttp.response);
+      }
+    };
+
+    xmlhttp.open(method,url,true);
+    xmlhttp.send();
+
+  } // closes ajaxRequest function
+
+  function updateClickCount(data){
+    var clicksObject = JSON.parse(data); // takes a string and truns into a JSON object
+    clickNbr.innerHTML = clicksObject.clicks; //gets the value from clicks property
   }
+
+  ready(ajaxRequest('GET', apiUrl, updateClickCount));
+
+  addButton.addEventListener('click',function(){
+
+      ajaxRequest('POST',apiUrl, function(){
+        ajaxRequest('GET',apiUrl,updateClickCount)
+      });
+
+  },false);// closes addButton event listener
+
+  deleteButton.addEventListener('click',function(){
+
+      ajaxRequest('DELETE',apiUrl,function(){
+        ajaxRequest("GET",apiUrl,updateClickCount);
+      });
+
+  },false);
 
 })();
