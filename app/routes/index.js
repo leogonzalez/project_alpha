c1651ff61833986d74dfe8ef30fdc179cdc31bd9
leogonzalez <leo.lujan.gonzalez@gmail.com>
2016-05-18 23:19:12 -0300

    added passport integration for second part of Clementine.js tutorial

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,15 +1,17 @@
 //Javascript file that contains the routes
 'use strict';
 
-var ClickHandler = require(process.cwd() + '/app/controllers/clickHandler.server.js');
+var path = process.cwd();
 
-module.exports = function(app,db){
+var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
 
-  var clickHandler= new ClickHandler(db);
+module.exports = function(app){
+
+  var clickHandler= new ClickHandler();
 
   app.route('/')
     .get(function(req,res){
-        res.sendFile(process.cwd()+'/public/index.html');
+        res.sendFile(path+'/public/index.html');
     });
 
   app.route('/api/clicks')
