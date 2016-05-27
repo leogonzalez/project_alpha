fa5d03fb744c9ce11111d052a8faf422b561c1b9
leogonzalez <leo.lujan.gonzalez@gmail.com>
2016-05-27 01:38:48 -0300

    update page 18

---
+++ app/config/auth.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    'githubaAuth': {
+        'clientID': process.env.GITHUB_KEY,
+        'clientSecret': process.env.GITHUB_SECRET,
+        'callbackURL':process.env.APP_URL + 'auth/github/callback'
+    }
+};
---
+++ app/models/users.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var user = new Schema({
+    github : {
+        id: String,
+        displayName: String,
+        username: String,
+        publicRepos: Number
+    },
+    nbrClicks: {
+        clicks: Number
+    }
+});
+
+module.exports = mongoose.model('User', User);
